46496b446270abcece8068af36945a59c9845da2
WOOD <48234074+mumom@users.noreply.github.com>
2019-11-10 15:10:11 +0700

    Add files via upload

---
+++ src/pro02/Pro02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pro02;
+
+/**
+ *
+ * @author ThisPC
+ */
+public class Pro02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new menu().setVisible(true);
+    }
+    
+}
